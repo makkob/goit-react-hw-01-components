5fadfa6a34b4c87c869f31cfb529b30b5a1fbd2d
makkob <maximkabig945@gmail.com>
2022-08-05 00:31:33 +0300

    HW

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import Profile from "./Profile";
 import Statistics from "./Statistics";
 import FriendList from "./FriendList";
+import TransactionHistory from "./TransactionHistory";
 import "./index.css";
 import user from "../data/user.json";
 import statistical from "../data/statistical-data.json";
 import friends from "../data/friends.json";
+import transaction from "../data/transactions.json";
 
 export default function App() {
   let { name, tag, location, avatar, stats } = user;
@@ -24,6 +26,7 @@ export default function App() {
         <Statistics stats={statistical} />
       </div>
       <FriendList friends={friends} />
+      <TransactionHistory transaction={transaction} />
     </>
   );
 }
---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./TransactionHistory.module.css";
+
+export default function TransactionHistory({ transaction }) {
+  return (
+    <table className={styles.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transaction.map(({ id, type, amount, currency }) => {
+          return (
+            <tr className={styles.tr} key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
