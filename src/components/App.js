bef66800f3766e535c3ee1058d03336fd499b28d
makkob <maximkabig945@gmail.com>
2022-08-29 21:54:30 +0300

    hw1

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,32 @@
 import React from "react";
-import user from "../data/user.json";
 import Profile from "./Profile";
+import Statistics from "./Statistics";
+import FriendList from "./FriendList";
+import TransactionHistory from "./TransactionHistory";
 import "./index.css";
+import user from "../data/user.json";
+import statistical from "../data/statistical-data.json";
+import friends from "../data/friends.json";
+import transaction from "../data/transactions.json";
 
 export default function App() {
   let { name, tag, location, avatar, stats } = user;
   return (
-    <Profile
-      name={name}
-      tag={tag}
-      location={location}
-      avatar={avatar}
-      stats={stats}
-    />
+    <>
+      <Profile
+        name={name}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+      <div>
+        {" "}
+        <Statistics title="Statistics" stats={statistical} />
+        <Statistics stats={statistical} />
+      </div>
+      <FriendList friends={friends} />
+      <TransactionHistory transaction={transaction} />
+    </>
   );
 }
---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from ".//FriendList.module.css";
+import PropTypes from "prop-types";
+
+export default function FriendList({ friends }) {
+  return (
+    <ul className={styles.friendsListUl}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li className={styles.item} key={id}>
+          <span className={isOnline ? styles.statusGreen : styles.statuRed}>
+            {" "}
+            {isOnline ? "Online" : "Offline"}
+          </span>
+          <br />
+          <img className="avatar" src={avatar} alt={name} width="48" />
+          <p className="name">{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.objectOf(
+      PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool])
+    )
+  ).isRequired,
+};
